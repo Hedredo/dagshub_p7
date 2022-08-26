a4059fb079f39007f3c7ce36667fb53cb500659f
Shambhavi Mishra <shambhavimishra26@gmail.com>
2022-08-26 18:28:51 +0000

    Update 'src/datacheck.py'

--- src/datacheck.py
+++ src/datacheck.py
@@ -1,26 +1,29 @@
+from importlib.resources import path
 import pandas as pd
 from deepchecks.tabular.suites import data_integrity
 from deepchecks.tabular import Dataset
 import argparse
 
 
-def datacheck(file_path):
+def datacheck(file_path, labels):
     data = pd.read_csv(file_path)
-    ds = Dataset(data)
+    ds = Dataset(data, label=labels)
     # Run Suite:
     integ_suite = data_integrity()
     suite_result = integ_suite.run(ds)
-    suite_result.save_as_html()
-    suite_result.show()
+    suite_result.to_json()
+
 
 # Instantiate the parser
 parser = argparse.ArgumentParser(description='Data Checks using DeepCheck')
-args = parser.parse_args()
-parser.add_argument('data_file', type=str,
+parser.add_argument('--file_path', type=str,
                     help='Path to your CSV file containing the data')
+parser.add_argument('--data_label', type=str, default='label',
+                    help='label')                    
 
 args = parser.parse_args()
-data = args.data_file
+data = args.file_path
+labels = args.data_label
 
 if __name__ == "__main__":
-    datacheck(data)
+    datacheck(data, labels)
